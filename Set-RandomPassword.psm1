1c782866ccdfd9ab3af303c72e3aec80a40f64ce
Connor McNamara <connor.mcnamara@oakland.k12.mi.us>
2022-04-25 12:27:38 -0400

    Added set password and credential auth

--- Set-RandomPassword.psm1
+++ Set-RandomPassword.psm1
@@ -1,3 +1,37 @@
+function Set-OSCredential
+{
+    param
+    (
+        $username
+    )
+
+    write-host "Enter your A account credentials"
+    # Allow the user to provide their username but if they don't just use the full get-credential dialog
+    if ($username)
+    {
+        $script:Cred = Get-Credential -UserName $username
+    }
+    else 
+    {
+        $script:Cred = Get-Credential   
+    }
+}
+
+<#
+    .SYNOPSIS 
+    Verifies a user has initialized the module with their admin credentials
+
+    .DESCRIPTION
+    Checks to see if the $cred variable has been set for this instance of the module. If it hasn't, run the set-oscredential function to get it
+#>
+function Test-OSCredentials
+{
+    if ($null -eq $Cred)
+    {
+        Set-OSCredential
+    }
+}
+
 function Get-RandomPassword
 {
     param (
@@ -42,4 +76,15 @@ function Get-RandomPassword
     }
 
     return $randomPW
+}
+
+function Set-RandomPassword 
+{
+    param 
+    (
+        $User,
+        $Password    
+    )
+    Test-OSCredentials
+    Set-ADAccountPassword -Identity $User -NewPassword (ConvertTo-SecureString -AsPlainText $Password -Force) -Credential $cred
 }
